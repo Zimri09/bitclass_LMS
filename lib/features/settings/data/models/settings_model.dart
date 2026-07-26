90f39e1f61c62ff6e80ffa714daff720946a7528
Zimri09 <zimri.logronio@bisu.edu.ph>
2026-07-26 19:08:52 +0800

    Default darkMode to true in AppSettingsModel

--- lib/features/settings/data/models/settings_model.dart
+++ lib/features/settings/data/models/settings_model.dart
@@ -7,7 +7,7 @@ class AppSettingsModel extends Equatable {
   final bool downloadOverWifiOnly;
 
   const AppSettingsModel({
-    this.darkMode = false,
+    this.darkMode = true,
     this.autoPlayVideos = false,
     this.downloadOverWifiOnly = true,
   });
@@ -40,7 +40,7 @@ class AppSettingsModel extends Equatable {
 
   factory AppSettingsModel.fromMap(Map<String, dynamic> map) {
     return AppSettingsModel(
-      darkMode: map['darkMode'] as bool? ?? false,
+      darkMode: map['darkMode'] as bool? ?? true,
       autoPlayVideos: map['autoPlayVideos'] as bool? ?? false,
       downloadOverWifiOnly: map['downloadOverWifiOnly'] as bool? ?? true,
     );
